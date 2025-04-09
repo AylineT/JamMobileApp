bdaeffb79432cbdcff252f6f88564075ff8cf86a
achrafthedev <achrafchardoudi2002@gmail.com>
2025-04-09 13:02:37 +0200

    using settings from config

--- back/app/core/database.py
+++ back/app/core/database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from app.core.config import settings
 import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
