7816b7516f1eb476902dcdcdc4be0cf9bef8e0da
Robin LE BROZEC <robin.lebrozec@gmail.com>
2025-04-10 15:03:06 +0200

    hotfix : change route name

--- back/app/routes/messages.py
+++ back/app/routes/messages.py
@@ -9,7 +9,7 @@ from datetime import datetime
 router = APIRouter()
 
 #Créer un message dans une conversation
-@router.post("/messages/", response_model=message_schema.MessageRead)
+@router.post("/", response_model=message_schema.MessageRead)
 def create_message(
     message: message_schema.MessageCreate,
     db: Session = Depends(get_db)
@@ -33,7 +33,7 @@ def create_message(
     return db_message
 
 #Récupérer les messages d'une conversation
-@router.get("/messages/{conversation_id}", response_model=list[message_schema.MessageRead])
+@router.get("/{conversation_id}", response_model=list[message_schema.MessageRead])
 def get_messages(conversation_id: int, db: Session = Depends(get_db)):
     messages = db.query(message_model.Message).filter_by(conversation_id=conversation_id).order_by(message_model.Message.timestamp).all()
     return messages
